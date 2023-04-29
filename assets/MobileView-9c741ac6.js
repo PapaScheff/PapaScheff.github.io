930f91b6202a2e247e55ae257d7fb214cba57162
Timur Lebedev <fsdf.fs.12@mail.ru>
2023-04-30 00:29:18 +0200

    0.1

---
+++ assets/MobileView-9c741ac6.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-be6f6d00.js";const n={};function r(t,a){return c(),o("h1",null,"Mobile page")}const _=e(n,[["render",r]]);export{_ as default};
---
+++ assets/MobileView-f9d704fa.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-2d9bb241.js";const n={};function r(t,a){return c(),o("h1",null,"Mobile page")}const _=e(n,[["render",r]]);export{_ as default};
